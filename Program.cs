bae390cdf9b85175648267c59843b82a6b010912
LipeClash <felipeclaudino2003@hotmail.com>
2020-05-21 02:45:14 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace auladepc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de soma");
+
+            String Primeironumero = Console.ReadLine();
+            String Segundonumero = Console.ReadLine();
+            String Resultado = Console.ReadLine();
+
+            Console.WriteLine($"{Primeironumero} + {Segundonumero} = {Resultado}");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Soma2Numeros
 22/05/2020
+
+feito
